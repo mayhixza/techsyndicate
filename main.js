bb38fd09a8103436182cc16edfdb984808b53b72
Varun Sharma <varun.pr0coder@gmail.com>
2021-07-14 23:55:27 +0530

    Basic auth

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
+const authRoutes = require('./routes/authRoutes');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -14,10 +16,11 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
+app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('index'));
+app.use(authRoutes);
 
 // Port
 app.listen(PORT, () => {
