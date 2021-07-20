7ea92285b73dfdb221ca06350a72d0ed5a08f999
mayhixza <emailwithmoi@gmail.com>
2021-07-21 03:17:15 +0530

    business traveller pages, routes, organised views

--- routes/travellerRoutes.js
+++ routes/travellerRoutes.js
@@ -6,20 +6,28 @@ const router = Router();
 
 // Routes
 router.get("/", (req, res) => {
-  res.render("tIndex");
+  res.render("traveller/tIndex");
 });
 // Make a post route here for "/" and forward to /detours
 
 router.get("/detours", (req, res) => {
-  res.render("detours");
+  res.render("traveller/detours");
 });
 
 router.get("/rewards", (req, res) => {
-  res.render("rewards");
+  res.render("traveller/rewards");
 });
 
 router.get("/started", (req, res) => {
-  res.render("started");
+  res.render("traveller/started");
+});
+
+router.get("/welcome", (req, res) => {
+  res.render("traveller/welcome");
+});
+
+router.get("/setup-detour", (req, res) => {
+  res.render("business/setup-detour");
 });
 
 // Middleware
