588e500349f4c8844e9b61e7650697bdb743d02f
Varun Sharma <varun.pr0coder@gmail.com>
2021-07-22 17:47:18 +0530

    Autocomplete for traveller form

--- routes/travellerRoutes.js
+++ routes/travellerRoutes.js
@@ -30,6 +30,88 @@ router.get("/setup-detour", (req, res) => {
   res.render("business/setup-detour");
 });
 
+//POST
+router.post("/search", async (req, res) => {
+  // const arr = [];
+  // const search = req.query.srch;
+  // const returnstr = "";
+  // const json = await fetch(
+  //   `https://atlas.mapmyindia.com/api/places/search/json?query=${search}`,
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Bearer b4230734-0bcb-4b2d-bda5-a12b2f4b9066",
+  //     },
+  //   }
+  // );
+  // let r = await json.json();
+  // console.log(r);
+  // if (r.suggestedLocations == []) {
+  //   res.status(404).json("not found lmfao");
+  // }
+  // for (var i = 0; i < 5 && r.suggestedLocations.length; i++) {
+  //   var obj = r.suggestedLocations[i];
+  //   arr.push(obj);
+  // }
+
+  // res.json(arr);
+
+res.json([
+  {
+    type: "CITY",
+    placeAddress: "Haryana",
+    eLoc: "122JUF",
+    placeName: "Gurugram",
+    alternateName: "Gurgaon,Gurgaun",
+    keywords: [],
+    orderIndex: 1,
+    suggester: "alternateName",
+  },
+  {
+    type: "DISTRICT",
+    placeAddress: "Haryana",
+    eLoc: "GILSMV",
+    placeName: "Gurugram District",
+    alternateName: "Gurgaon District",
+    keywords: [],
+    orderIndex: 2,
+    suggester: "alternateName",
+  },
+  {
+    type: "POI",
+    placeAddress:
+      "Gurgaon Farukh Nagar Road, Farrukhnagar, Gurugram District, Haryana, 122505",
+    eLoc: "6BTF3C",
+    placeName: "Sultanpur National Park",
+    alternateName: "Sultanpur Bird Sanctuary,Haryana Birds Paradise",
+    keywords: ["NTCBDS"],
+    orderIndex: 3,
+    suggester: "placeName",
+  },
+  {
+    type: "POI",
+    placeAddress: "Gurgaon, Gurgaon Village, Gurugram, Haryana, 122006",
+    eLoc: "A4M7KC",
+    placeName: "Gurgaon Village 3",
+    alternateName: "",
+    keywords: ["HLTHSP"],
+    orderIndex: 4,
+    suggester: "placeName",
+  },
+  {
+    type: "POI",
+    placeAddress:
+      "Gurgaon Masani Road, Gurgaon Village, Gurugram, Haryana, 122006",
+    eLoc: "IYLW0E",
+    placeName: "Gurgaon Media Works",
+    alternateName: "",
+    keywords: ["SHPMAD"],
+    orderIndex: 5,
+    suggester: "placeName",
+  },
+]);
+});
 // Middleware
 
 module.exports = router;
